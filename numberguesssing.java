1dd7f491eaf0562f41072fdde1ce94f559920331
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-08-27 20:34:25 +0530

    java: number guessing game

---
+++ numberguesssing.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class numberguesssing {
+    public static void main(String[] args){
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+
+        int guess;
+        int attempts = 0;
+        int randomNumber = random.nextInt(1, 11);
+
+        System.out.println("Number guessing game");
+        System.out.print("Guess a number between 1-10 : ");
+
+        do {
+            System.out.print("Enter a guess: ");
+            guess= sc.nextInt();
+            attempts+=1;
+
+            if (guess < randomNumber){
+                System.out.println("too low try higher");
+            }else if (guess > randomNumber){
+                System.out.println("too high try lower");
+            }else{
+                System.out.println("you cracked it");
+                System.out.println("# of attempts: " + attempts);
+            }
+        }while(guess != randomNumber);
+
+        System.out.println("you have won !!");
+        sc.close();
+    }
+}
