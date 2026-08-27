a2872d63a68b1dc231c64c6a5391c829d9284e2a
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-27 22:24:12 +0530

    java: happy new year counter

---
+++ happynewyearcounter.java
@@ -0,0 +1,16 @@
+
+public class happynewyearcounter{
+
+  public static void main(String[] args){
+
+
+    int a = 10;
+
+    for (int i = 10; i>=0; i--){
+      System.out.println(i);
+    }
+
+    System.out.println("happy new year !!");
+  }
+}
+
