a126c9d8666e9263d9bf5859d8d80674ad664938
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-08-27 19:58:31 +0530

    java: while loops

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class whileloop{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String name = "";
+
+    while (name.isEmpty()){
+      System.out.print("enter your name: ");
+      name = sc.nextLine();
+    }
+
+
+    System.out.println("hello "+ name);
+    sc.close();
+  }
+}
