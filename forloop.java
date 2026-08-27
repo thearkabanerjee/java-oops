fe3e9c52dd8fce0ef4d323de88885bce03376ef5
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-27 22:14:59 +0530

    java: for loops

---
+++ forloop.java
@@ -0,0 +1,14 @@
+
+
+public class forloop{
+  public static void main(String[] args){
+    int a = 12;
+    
+    for (int i = 0; i < a; i++){
+      System.out.println(i+1 + " this is number now");
+    }
+  
+
+    
+  }
+}
