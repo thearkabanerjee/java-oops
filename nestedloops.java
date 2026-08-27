8e369a12b21376a5d2175ad3672fcdd1ef776e3d
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-28 00:14:34 +0530

    java: nested loops

---
+++ nestedloops.java
@@ -0,0 +1,14 @@
+
+
+public class nestedloops{
+  public static void main(String[] args){
+
+    int a = 3;
+    
+    for (int i = 0; i < a; i++){
+      for (int j = 0; j < 10; j++){
+        System.out.print(j + " ");
+      }
+    }
+  }
+}
