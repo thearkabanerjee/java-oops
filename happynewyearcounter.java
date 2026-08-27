606708e445abe414483fff243d174bbc1afd4eaf
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-27 22:29:20 +0530

    java: happy new year counter

--- happynewyearcounter.java
+++ happynewyearcounter.java
@@ -1,13 +1,14 @@
 
 public class happynewyearcounter{
 
-  public static void main(String[] args){
+  public static void main(String[] args)throws InterruptedException{
 
 
     int a = 10;
 
-    for (int i = 10; i>=0; i--){
+    for (int i = 10; i>0; i--){
       System.out.println(i);
+      Thread.sleep(1000);
     }
 
     System.out.println("happy new year !!");
