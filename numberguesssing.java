48b9d88e73bb2b2dbc715367a21dc7b94732cd5b
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-08-27 20:35:03 +0530

    java: number guessing game

--- numberguesssing.java
+++ numberguesssing.java
@@ -8,7 +8,9 @@ public class numberguesssing {
 
         int guess;
         int attempts = 0;
-        int randomNumber = random.nextInt(1, 11);
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max+1);
 
         System.out.println("Number guessing game");
         System.out.print("Guess a number between 1-10 : ");
