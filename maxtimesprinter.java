ca12471b32bec274f73e3176fb3c2ddbebdb5572
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-27 22:19:33 +0530

    java: maxtimesprinter

---
+++ maxtimesprinter.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+
+public class maxtimesprinter{
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the max number of times you want it to loop : ");
+
+    int max = sc.nextInt();
+
+    for (int i = 0; i <max; i++){
+      System.out.println(i);
+    }
+    
+    sc.close();
+
+  }
+}
