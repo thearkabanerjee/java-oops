095bb5d8ebbf31e5cd91d33bde59ed626211011a
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-28 13:07:03 +0530

    java : variable scope

---
+++ variablescope.java
@@ -0,0 +1,19 @@
+public class variablescope {
+    static int x = 3; // class
+
+    public static void main(String[] args){
+        int x = 1; // local variable
+        System.out.println(x); // prints the value 1 
+        dosomething(); // prints the value 2
+        printValue(); // prints the global variable 
+    }
+
+    static void dosomething(){
+        int x = 2; // local variable
+        System.out.println(x);
+    }
+
+    static void printValue(){
+        System.out.println(x);
+    }
+}
