a3213b8372dd12fb34d1d098024318ec4f2c5fe3
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-28 11:51:09 +0530

    java: methods

--- methods.java
+++ methods.java
@@ -5,15 +5,16 @@ public class methods {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
+        int age = sc.nextInt();
         for (int i = 3; i > 0; i--){
-            happybirthday(name);
+            happybirthday(name, age);
         }
         sc.close();
     }
-    public static void happybirthday(String name){
-        System.out.println("happy birthday to you");
+    public static void happybirthday(String name, int age){
         System.out.println("happy birthday to you");
         System.out.println("happy birthday dear "+ name);
+        System.out.printf("you are %d years old\n", age);
         System.out.println("happy birthday to you");
     }
 }
