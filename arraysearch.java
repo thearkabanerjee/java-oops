cdca9cbad675c986418674e4fbb979e2a1b7c255
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-28 16:58:13 +0530

    java : array search

--- arraysearch.java
+++ arraysearch.java
@@ -3,16 +3,19 @@ public class arraysearch {
         int[] numbers = {1, 9, 3, 4, 2, 8, 5, 42};
         int target = 42;
         boolean found = false;
+        int index = 0;
 
 
         for (int i = 0; i < numbers.length; i++){
             if (numbers[i] == target){
                 found =true;
+                index = i;
             }
         }
 
         if (found){
             System.out.println("found in the array");
+            System.out.println("the index is "+ index);
         }else{
             System.out.println("not in the array");
         }
