1c84fd9be66aab48597df6ae99a1ecd33e87eedd
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-03 01:59:57 +0530

    java : varargs.java

---
+++ oppe/question1.java
@@ -0,0 +1,5 @@
+package oppe;
+
+public class question1 {
+    
+}
---
+++ vargs.java
@@ -0,0 +1,16 @@
+public class vargs {
+    public static void main(String[] args){
+        // varargs = allow a method to accept a varying number of arguments
+        System.out.println(add(1, 2,3, 4));
+    }
+    static int add(int... numbers){
+        int sum = 0;
+        for (int number: numbers){
+            sum += number;
+        }
+        return sum;
+    }
+}
+
+
+// varargs is very important , used incase one needs to change up the number of arguments they want to put in
