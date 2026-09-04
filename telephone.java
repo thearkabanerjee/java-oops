91920764d5b78fb7cb590ea2ccdf9c798481be16
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-04 05:30:44 +0530

    java : telephone

---
+++ telephone.java
@@ -0,0 +1,12 @@
+public class telephone {
+    public static void main(String[] args){
+        char[][] telephone = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
+
+        for (char[] row : telephone){
+            for (char number : row){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+}
