ae563bae3ee6742a977aac69c15e26c0b5b8257d
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-04 21:59:57 +0530

    java

---
+++ question3/Main.java
@@ -0,0 +1,10 @@
+package question3;
+
+public class Main {
+    public static void main(String[] args){
+        Student student1 = new Student(21, "Arka");
+
+        System.out.println(student1.name + " " + student1.age);
+        
+    }
+}
