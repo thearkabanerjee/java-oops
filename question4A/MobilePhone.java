27969696c3d77abba806d21ef1013a8d5b84bd97
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-04 22:45:21 +0530

    java

---
+++ question4A/MobilePhone.java
@@ -0,0 +1,21 @@
+package question4A;
+
+public class MobilePhone {
+    private String brand;
+    private double price;
+
+    public MobilePhone(String brand, double price){
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+
+}
+
