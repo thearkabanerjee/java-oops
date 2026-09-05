694bf0719eedcaba4b0b05f6485d6d9ddcba5ce1
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-05 10:35:49 +0530

    java

---
+++ practicejava/rockpaper.java
@@ -0,0 +1,23 @@
+package practicejava;
+// import java.util.*;
+
+// public class rockpaper {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         // rock paper scissors game headline
+//         System.out.println("********************");
+//         System.out.println("Rock Paper Scissiors");
+//         System.out.println("********************");
+
+//         System.out.print("Enter your choice: ");
+        
+//         Random random = new Random();
+
+//         String[] choices = {"rock", "paper", "scissors"};
+//         String choice = sc.nextLine();
+
+
+//         sc.close();
+//     }
+// }
