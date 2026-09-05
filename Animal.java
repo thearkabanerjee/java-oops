bec1c417a31b93d658f864dc374a84944188b088
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-05 12:00:35 +0530

    main

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    boolean isAlive;
+    Animal(){
+        isAlive = true;
+    }
+    void eat(){
+        System.out.println("the animal is eating");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        cat.eat();
+        dog.eat();
+
+    }
+}
